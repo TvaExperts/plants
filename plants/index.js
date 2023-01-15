da989889fff5591715c9bfd3150d574885829e45
Viktor Trachenko <trachenko.v.a@gmail.com>
2023-01-16 00:41:02 +0300

    feat: end 2 part plants

--- plants/index.js
+++ plants/index.js
@@ -1,5 +1,26 @@
 document.addEventListener('DOMContentLoaded', function () {
+
     document.getElementById('burger').addEventListener('click', function() {
-        document.querySelector('header').classList.toggle('open');
+        document.querySelector('.header__navigation').classList.toggle('_open');
+        
     })
+
+    const menuLinks=document.querySelectorAll('.navigation__link');
+
+    menuLinks.forEach(menuLink => {
+        menuLink.addEventListener('click', function() {
+            document.querySelector('.header__navigation').classList.remove('_open');
+        });
+    })
+    
+    document.addEventListener('click', e => {
+        if (! document.querySelector('.header__navigation').contains(e.target)) {
+            document.querySelector('.header__navigation').classList.remove('_open');
+        }
+    })
+
 })
+
+
+
+
